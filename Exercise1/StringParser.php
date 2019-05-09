9eb8768240ddd7fc7f5aaf4c4181763f8c8cb501
Justin Kannekens <justinkannekens@gmail.com>
2019-05-09 12:17:09 +0200

    Created function of exercise 1

--- Exercise1/StringParser.php
+++ Exercise1/StringParser.php
@@ -1,7 +1,11 @@
 <?php
 $sentences = file("input.txt");
-foreach ($sentences as $value) {
-    $purified = preg_replace('/[^a-zA-Z]/',' ', $value);
-    echo "$purified <br>";
+function StringParser($array) {
+    foreach ($array as $value) {
+        $purified = preg_replace('/[^a-zA-Z]/',' ', $value);
+        echo "$purified <br>";
+    }
 }
+
+StringParser($sentences);
 ?>
